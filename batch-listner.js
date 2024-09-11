dc22c0ec5efb9006013e91a0fea8943e6b83c596
harshal-42 <ext-harshal.pandit@lahitapiola.fi>
2024-09-11 23:26:20 +0530

    Added file via Upload

---
+++ batch-listner.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.post('/batch', (req, res) => {
+
+  const message = req.body.data;
+
+  console.log('Received batch message:', message);
+
+  // Simulate processing the batch
+
+  res.status(200).send('Batch processed');
+
+});
+
+
+
+app.listen(3000, () => {
+
+  console.log('Batch listener running on port 3000');
+
+});
